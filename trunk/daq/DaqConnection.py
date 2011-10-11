821a8489b0d265870c2ffd2ef74211cd121d75bf
achim.stoessl <achim.stoessl@1666bff2-7c83-11de-a0b6-3f18d6cea68c>
2011-10-11 14:23:45 +0000

    * Broken serial connections should now be captured

--- trunk/daq/DaqConnection.py
+++ trunk/daq/DaqConnection.py
@@ -5,7 +5,7 @@ import serial
 
 class DaqConnection(object):
 
-    def __init__(self, inqueue, outqueue, debug):
+    def __init__(self, inqueue, outqueue, logger):
 
         try:
             self.port = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, bytesize=8,parity='N',stopbits=1,timeout=0.5,xonxoff=True)
@@ -15,7 +15,28 @@ class DaqConnection(object):
         self.inqueue = inqueue
         self.outqueue = outqueue
         self.running = 1
-        self.debug = debug
+        self.logger = logger
+
+    def get_port():
+        connected = False
+        while not connected:
+            dev = subprocess.Popen(['which_tty_daq'], stdout=subprocess.PIPE).communicate([0])
+            dev = "/dev/" + dev
+            dev = dev.rstrip('\n')
+            self.logger.info("Daq connected to %s",dev)
+            try:
+                port = serial.Serial(port=dev, baudrate=115200,
+                                     bytesize=8,parity='N',stopbits=1,
+                                     timeout=1,xonxoff=True)
+                connected = True
+            except serial.SerialException, e:
+                logger.error(e)
+                logger.error("Waiting 10 seconds")
+                time.sleep(10)
+
+        self.logger.info("Successfully connected to serial port")
+        return port
+
 
 
     def read(self):
@@ -35,13 +56,31 @@ class DaqConnection(object):
 #                data += self.port.read(n)
 #            for line in data.split('\n'):
 #                self.outqueue.put(line)
-            if self.port.inWaiting():
-                while self.port.inWaiting():
-                    self.outqueue.put(self.port.readline().strip())
-                sleeptime = max(sleeptime/2, min_sleeptime)
-            else:
-                sleeptime = min(1.5 * sleeptime, max_sleeptime)
-            time.sleep(sleeptime)
+            try:
+                if self.port.inWaiting():
+                    while self.port.inWaiting():
+                        self.outqueue.put(self.port.readline().strip())
+                    sleeptime = max(sleeptime/2, min_sleeptime)
+                else:
+                    sleeptime = min(1.5 * sleeptime, max_sleeptime)
+                time.sleep(sleeptime)
+
+            except IOError:
+                self.logger.error("IOError")
+                self.port.close()
+                self.port = self.get_port()
+                # this has to be implemented in the future
+                # for now, we assume that the card does not forget
+                # its settings, only because the USB connection is
+                # broken
+                #self.setup_daq.setup(self.commandqueue)
+            except OSError:
+                self.logger.error("IOError")
+                self.port.close()
+                self.port = self.get_port()
+                #self.setup_daq.setup(self.commandqueue)
+
+
 
     def write(self):
         while self.running:
