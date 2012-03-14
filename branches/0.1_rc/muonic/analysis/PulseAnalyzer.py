3e8fcecfa39870dc852b56dfcd9ecc8c9fe50eb8
achim.stoessl@gmail.com <achim.stoessl@gmail.com@1666bff2-7c83-11de-a0b6-3f18d6cea68c>
2012-03-14 13:14:09 +0000

    MAJOR: The cpld frequency is now calculated automatically by using the onepps gps information        This is important, since such drifts could easily result in an uncertainty of the        trigger time information of about 0.5 microsec.        So gps is needed for muondecay... MINOR: The functionality of get_time is included into PulseAnalyzer.py and got a speed up        It seems now that we do things more right in Pulseextraction, but tests are needed..
    ISSUE: triggers without pulses still occur! It seems that these triggers would contain only
           single rising or falling edges, this has to be investigated, maybe there is another
           clock correction mechanism we did not take into account yet

--- branches/0.1_rc/muonic/analysis/PulseAnalyzer.py
+++ branches/0.1_rc/muonic/analysis/PulseAnalyzer.py
@@ -1,5 +1,7 @@
 #! /usr/env/bin python
 
+
+
 # for the pulses 
 # 8 bits give a hex number
 # but only the first 5 bits are used for the pulses time,
@@ -15,16 +17,18 @@ BIT1 = 1 << 1 # Trigger interrupt pending
 BIT2 = 1 << 2 # GPS data possible corrupted
 BIT3 = 1 << 3 # Current or last 1PPS rate not within range
 
-#assuming the cpld clock runs with approx 41MHz
-cpld_tick = 24  #nsec 
+
+# ticksize of tmc internal clock
+# TODO: find out if tmc is coupled to cpld!
 tmc_tick = 0.75 #nsec
 
-# get the pulses out of a daq line
-# we have to be as fast as possible!
 
-from get_time import get_time
 
 class PulseExtractor:
+    """
+    get the pulses out of a daq line
+    we have to be as fast as possible!
+    """
 
     def __init__(self,pulsefile=''):
         """
@@ -49,7 +53,7 @@ class PulseExtractor:
         self.lastchan3re = []
         self.lastchan3fe = []
         
-        self.trigger = 0
+        self.lasttriggertime = 0
 
         self.pulsefile = pulsefile
         if pulsefile:
@@ -59,6 +63,17 @@ class PulseExtractor:
         # trigger
         self.ini = True
 	self.lastonepps = 0.
+        self.gps_evttime = 0.
+
+
+        # variables for DAQ frequency calculation
+        self.lastfrequencypolltime = 0
+        self.lastfrequencypolltriggers = 0
+        self.calculatedfrequency = 41.0e6
+        self.defaultfrequency = 41.0e6
+        self.pollcount = 0
+        self.lastoneppspoll = 0
+        self.passedonepps = 0
 
     def calculate_edges(self,line,thistrigger=False):
 
@@ -74,34 +89,36 @@ class PulseExtractor:
         
 
         if (re0 & BIT5):    
-            self.chan0re.append((self.linetime - self.trigger) + (re0 & BIT0_4)*tmc_tick)  
+            self.chan0re.append((self.linetime - self.lasttriggertime) + (re0 & BIT0_4)*tmc_tick)  
         if (fe0 & BIT5):
-            self.chan0fe.append((self.linetime - self.trigger) + (fe0 & BIT0_4)*tmc_tick)
+            self.chan0fe.append((self.linetime - self.lasttriggertime) + (fe0 & BIT0_4)*tmc_tick)
         if (re1 & BIT5):
-            self.chan1re.append((self.linetime - self.trigger) + (re1 & BIT0_4)*tmc_tick)
+            self.chan1re.append((self.linetime - self.lasttriggertime) + (re1 & BIT0_4)*tmc_tick)
         if (fe1 & BIT5):
-            self.chan1fe.append((self.linetime - self.trigger) + (fe1 & BIT0_4)*tmc_tick)
+            self.chan1fe.append((self.linetime - self.lasttriggertime) + (fe1 & BIT0_4)*tmc_tick)
         if (re2 & BIT5):
-            self.chan2re.append((self.linetime - self.trigger) + (re2 & BIT0_4)*tmc_tick)
+            self.chan2re.append((self.linetime - self.lasttriggertime) + (re2 & BIT0_4)*tmc_tick)
         if (fe2 & BIT5):
-            self.chan2fe.append((self.linetime - self.trigger) + (fe2 & BIT0_4)*tmc_tick)
+            self.chan2fe.append((self.linetime - self.lasttriggertime) + (fe2 & BIT0_4)*tmc_tick)
         if (re3 & BIT5):
-            self.chan3re.append((self.linetime - self.trigger) + (re3 & BIT0_4)*tmc_tick)
+            self.chan3re.append((self.linetime - self.lasttriggertime) + (re3 & BIT0_4)*tmc_tick)
         if (fe3 & BIT5):
-            self.chan3fe.append((self.linetime - self.trigger) + (fe3 & BIT0_4)*tmc_tick)
+            self.chan3fe.append((self.linetime - self.lasttriggertime) + (fe3 & BIT0_4)*tmc_tick)
+
+
+
 
     def order_and_cleanpulses(self):
         """
         Remove pulses which have a 
         leading edge later in time than a 
         falling edge and do a bit of sorting
+        Remove also single leading or falling edges
         """
-
 
         self.chan0 = zip(self.lastchan0re,self.lastchan0fe)
         for i in self.chan0:
             if not i[0] < i[1]:
-                #print 'removed', i
                 self.chan0.remove(i)
            
         self.chan1 = zip(self.lastchan1re,self.lastchan1fe)
@@ -119,27 +136,83 @@ class PulseExtractor:
             if not i[0] < i[1]:
                 self.chan3.remove(i)
 
+    def get_gps_time(self,time, correction):
+        '''
+        Convert hhmmss,xxx string int seconds since day start
+        '''
+
+        tfields = time.split(".")
+        t = tfields[0]
+        secs_since_day_start = int(t[0:2])*3600+int(t[2:4])*60+int(t[4:6])
+        evt_time = secs_since_day_start + int(tfields[1])/1000.0+int(correction)/1000.0
+        self.gps_evttime = evt_time
+        return evt_time
+    
+
+
+
+    def get_time(self,trigger_count):
+        
+        line_time = self.gps_evttime + (trigger_count - self.lastonepps)/self.calculatedfrequency    
+        return line_time
+
+    def get_daq_frequency(self,onepps):
+        """
+        time: in seconds
+        triggers: in counts
+        """
+        if not self.passedonepps:
+            self.calculatedfrequency = self.defaultfrequency
+            return
+
+        frequency = (onepps - self.lastoneppspoll)/self.passedonepps
+        self.calculatedfrequency = float(frequency)
+        self.lastoneppspoll = onepps
+        if not frequency:
+            self.calculatedfrequency = self.defaultfrequency
+
+
     def extract(self,line):
         """Search for triggers in a set of lines"""
 
         line = line.split()
 
+        # remember this!
+        # 92328FE2 00 3D 00 3E 00 00 00 00 915E10CF 034016.021 060180 V 00 0 +0055
+        # triggers r0 f0 r1 f1 r2 f2 r3 f3 onepps   gpstime    gpsdte        correction
+        #  0        1  2  3  4  5  6  7  8    9       10        11    12 13 14 15
 
-        #self.linetime = int(line[0],16)*cpld_tick
         # remember here that get_time returns seconds!
-        self.linetime = get_time(line,self.lastonepps)*1e9
-        self.lastonepps = int(line[9],16)
+        # we need nanoseconds here, so that we can add the pulses le 
+        # and fe timet        
+
+        onepps        = int(line[9],16)
+        trigger_count = int(line[0],16)
+
+        if onepps != self.lastonepps:
+            self.lastonepps = onepps
+            self.passedonepps += 1 
+
+        self.get_gps_time(line[10],line[15])
+        self.linetime = self.get_time(trigger_count)*1e9
+        
+
+
         self.triggerflag = int(line[1],16) & BIT7    
 
+        # poll every x lines for the frequency
 
+        self.pollcount += 1
+        if not self.pollcount%50:
+            self.get_daq_frequency(onepps)
+            self.pollcount = 0
+            self.passedonepps = 0
   
-        #if self.triggerflag >= 80:
         if self.triggerflag:
         
             self.ini = False
             
             # a new trigger!o we have to evaluate the last one and get the new pulses
-            self.lasttriggertime = self.trigger
             self.lastchan0re = self.chan0re
             self.lastchan0fe = self.chan0fe
             self.lastchan1re = self.chan1re
@@ -148,9 +221,22 @@ class PulseExtractor:
             self.lastchan2fe = self.chan2fe
             self.lastchan3re = self.chan3re
             self.lastchan3fe = self.chan3fe
+            self.order_and_cleanpulses()
+            extracted_pulses = (self.lasttriggertime,self.chan0,self.chan1,self.chan2,self.chan3) 
 
+            # UNCOMMENT FOR DEBUG
+            #i = 0
+            #for item in extracted_pulses[1:]:
+            #     if item:
+            #         i += 1
 
-            self.trigger = self.linetime
+            #if not i:
+            #    print line
+            #    print extracted_pulses 
+
+            # the pulses of the last event are done!
+            # now the new beginning event
+            self.lasttriggertime = self.linetime
 
             self.chan0re = []  
             self.chan0fe = []
@@ -162,9 +248,20 @@ class PulseExtractor:
             self.chan3fe = []         
 
             self.calculate_edges(line,thistrigger=True)
-            self.order_and_cleanpulses()
 
-            extracted_pulses = (self.lasttriggertime,self.chan0,self.chan1,self.chan2,self.chan3) 
+            # UNCOMMENT FOR DEBUG
+            #print self.chan0re
+            #print self.chan0fe
+            #print self.chan1re
+            #print self.chan1fe
+            #print self.chan2re
+            #print self.chan2fe
+            #print self.chan3re
+            #print self.chan3fe
+
+            
+
+
             if self.pulsefile:
                 self.pulsefile.write(extracted_pulses.__repr__() + '\n')
 
@@ -177,11 +274,22 @@ class PulseExtractor:
 
             if self.ini:
                 self.lastonepps = int(line[9],16)
-                return None
 
 
             else:
                 self.calculate_edges(line)
+                # UNCOMMENT FOR DEGUB
+                #print 'Adding more pulses' 
+                #print self.chan0re
+                #print self.chan0fe
+                #print self.chan1re
+                #print self.chan1fe
+                #print self.chan2re
+                #print self.chan2fe
+                #print self.chan3re
+                #print self.chan3fe
+               
+
 
 
     def close_file(self):
@@ -231,7 +339,7 @@ class DecayTrigger:
                decaytime = thistriggerpulses[0] - self.lasttriggerpulses[0]
                #print decaytime
                if decaytime < self.triggerwindow:
-                   print 'We registered a decayed muon'
+                   #print 'We registered a decayed muon'
                    self.lasttriggerpulses = thistriggerpulses
                    if not decaytime < 0:
                        return decaytime
@@ -290,12 +398,16 @@ if __name__ == '__main__':
     data = open(sys.argv[1])
     extractor = PulseExtractor()
     while True:
+        line = data.readline()
+        if not line:
+	    break
         try:
-            print extractor.extract(data.readline())
+            extractor.extract(line)
+            #print extractor.extract(line)
             #print extractor.chan1fe
-        except ValueError:
-            print 'VE'
+        except (ValueError,IndexError):
+            #print 'some error', line
 
-
+            pass 
 
 
